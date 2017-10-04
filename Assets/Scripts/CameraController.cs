2f5c401a2305fed843f9b2000e328b9a49f521ae
Franlyn <hya613@msn.cn>
2017-10-04 14:50:06 -0400

    move the camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+	// Guaranteed to update after all processes are done
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
